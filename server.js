db597e89bba99d484ae74187a2d728f71a177649
J. Taylor Allen <j.taylorallen@yahoo.com>
2022-01-29 17:25:35 -0600

    a little lost

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const notes = require('./db/db.json')
+// const routes = require('./routes/route')
+
+const PORT = process.env.PORT || 3000;
 
 app.get ('/api/notes', (req, res) => {
   res.json(notes);
-})
+});
 
+app.post('/api/notes', (req, res) => {
+  
+});
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+// app.delete ???
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
