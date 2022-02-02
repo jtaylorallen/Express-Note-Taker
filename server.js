f89448a03e796c612a7219394977654f81d1c27e
J. Taylor Allen <j.taylorallen@yahoo.com>
2022-02-02 05:56:01 -0600

    added code from  module for ref, will del later

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
-const app = express();
+const fs = require('fs');
+const path = require('path')
 const notes = require('./db/db.json')
 // const routes = require('./routes/route')
 
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+
 
 app.get ('/api/notes', (req, res) => {
   res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-  
+  // set id based on what the next index of the array will be
+  req.body =
+
+  res.json(notes);
 });
 
 // app.delete ???
