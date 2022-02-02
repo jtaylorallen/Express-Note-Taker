8eea8d5758600cf623ba3619c84f61b30e2ad0b1
J. Taylor Allen <j.taylorallen@yahoo.com>
2022-02-02 05:59:05 -0600

    del zoo ref, changes to htmlrout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,11 @@
 const path = require('path');
 const router = require('express').Router();
 
-
-// from zoo keeper
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  
+  router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
   });
   
-  router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/animals.html'));
-  });
-  
-  router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
-  });
   
   router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
